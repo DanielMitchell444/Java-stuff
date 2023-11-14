43b9d5f8ecd9aaa632d0988020d99f05e89ba022
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2023-11-14 06:41:05 -0800

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,11 @@
+class problem1{
+ public static void main(string[]args){
+ //The goal of this problem is to make a int, and a double variable
+ //and print it on the screen
+
+ //Declare two variables using the proper naming convention, the 
+ //variables can be any numbers
+
+ //Print both the integer variable and the double variable on the screen.
+ }
+}
---
+++ problem2.java
@@ -0,0 +1,17 @@
+class problem2{
+ public static void main(string [] args){
+    //In this problem, you will guess the answer using comments
+
+    int x = 5;
+    int y = 10;
+
+    x = y;
+
+    y = 3;
+
+    System.out.println(y);
+
+    //What will Y equal?
+    //TYPE ANSWER HERE: 
+ }
+}
